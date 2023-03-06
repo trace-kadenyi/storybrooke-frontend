94c27fc0843681b1aa88f50cf626260cc9480786
kadenyi <treykadenyi@gmail.com>
2023-03-06 15:14:08 +0300

    Fetch register user from store and add functionality to register user on the form

--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { registerUser } from "../../Redux/userSlice";
 
 import "./registration.css";
 
 const Registration = () => {
+  const dispatch = useDispatch();
+  const { user, status } = useSelector((state) => state.user);
+
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    if (status === "success") {
+      alert("Registration successful");
+      setName("");
+      setPassword("");
+    } else if (status === "failed") {
+      alert("User already exists. Try another name.");
+    }
+  }, [status]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newUser = {
+      user: name,
+      pwd: password,
+    };
+    dispatch(registerUser(newUser));
+  };
+
   return (
     <section className="registration">
       <h1>Registration</h1>
-      <form className="register_form">
+      <form className="register_form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name</label>
           <input
@@ -14,6 +41,8 @@ const Registration = () => {
             name="name"
             id="name"
             placeholder="Enter your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             required
           />
         </div>
@@ -24,6 +53,8 @@ const Registration = () => {
             name="password"
             id="password"
             placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             required
           />
         </div>
