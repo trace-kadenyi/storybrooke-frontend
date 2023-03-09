7dca28928fe83add19d9dc8d057d990ef06347b9
kadenyi <treykadenyi@gmail.com>
2023-03-09 12:04:07 +0300

    Add authProvider

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import store from './Redux/store';
+import { AuthProvider } from './Context/AuthProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <AuthProvider>
       <App />
-    </Provider>
+    </AuthProvider>
   </React.StrictMode>
 );
