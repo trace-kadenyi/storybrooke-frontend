93fc288bb6fa6bcfa97eff36b6323beaa3ad816e
kadenyi <treykadenyi@gmail.com>
2023-03-10 01:34:53 +0300

    remove css

--- src/Components/Registration/Register.js
+++ src/Components/Registration/Register.js
@@ -5,7 +5,7 @@ import axios from "../../Api/axios";
 
 // import { registerUser } from "../../Redux/registerUserSlice";
 
-import "./register.css";
+// import "./register.css";
 
 const Register = () => {
   // const dispatch = useDispatch();
