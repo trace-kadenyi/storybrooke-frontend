84599120d368d68b99f94e985318a18243396aa9
Tracey Kadenyi <treykadenyi@gmail.com>
2023-03-21 21:49:05 +0300

    Style the page

--- src/Components/Registration/Register.js
+++ src/Components/Registration/Register.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import axios from "../../Api/axios";
 
 // import { registerUser } from "../../Redux/registerUserSlice";
 
-// import "./register.css";
+import "./register.css";
 
 const Register = () => {
   // const dispatch = useDispatch();
   // const { user, status } = useSelector((state) => state.user);
 
+  const navigate = useNavigate();
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
@@ -21,41 +22,8 @@ const Register = () => {
   const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
   useEffect(() => {
-    setResponse("")
-  }, [name, password, confirmPassword])
-
-  // handle status change
-  // useEffect(() => {
-  //   if (status === "loading") {
-  //     setResponse("Loading...");
-  //   } else if (status === "success") {
-  //     setResponse(`User ${name} created successfully. Login to continue...`);
-  //     setName("");
-  //     setPassword("");
-  //     setConfirmPassword("");
-  //     setSuccess(true);
-  //   } else if (status === "failed") {
-  //     setResponse(`User ${name} already exists. Please try another name.`);
-  //   }
-  // }, [status]);
-
-  // handle form submit
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   const newUser = {
-  //     user: name,
-  //     pwd: password,
-  //   };
-  //   if (!PWD_REGEX.test(password)) {
-  //     setResponse(
-  //       "Password must be 8-24 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character"
-  //     );
-  //   } else if (password !== confirmPassword) {
-  //     setResponse("Passwords do not match");
-  //   } else {
-  //     dispatch(registerUser(newUser));
-  //   }
-  // };
+    setResponse("");
+  }, [name, password, confirmPassword]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -84,19 +52,8 @@ const Register = () => {
       setName("");
       setPassword("");
       setConfirmPassword("");
-      // if (!PWD_REGEX.test(password)) {
-      //   setResponse(
-      //     "Password must be 8-24 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character"
-      //   );
-      // } else if (password !== confirmPassword) {
-      //   setResponse("Passwords do not match");
-      // } else {
-      //   console.log(JSON.stringify(response?.data));
-      //   setSuccess(true);
-      //   setName("");
-      //   setPassword("");
-      //   setConfirmPassword("");
-      // }
+      navigate("/login", { replace: true });
+      alert("Registration successful.");
     } catch (error) {
       if (!error.response) {
         setResponse("No response from server");
@@ -109,67 +66,93 @@ const Register = () => {
   };
 
   return (
-    <>
-      {success ? (
-        <section className="succesful_registration">
+    <section className="registration">
+      <header className="login_header">
+        <nav>
+          <img
+            src="https://www.freepnglogos.com/uploads/instagram-logo-png-transparent-0.png"
+            alt="logo"
+            className="logo"
+          />
+          <ul>
+            <li>
+              <NavLink to="/about" className="link">
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/login" className="link">
+                Login
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/profile" className="link">
+                Welcome
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div className="login_sect">
+        <form className="register_form" onSubmit={handleSubmit}>
           <div>
-            <h1>Registration Successful</h1>
-            <Link to="/login">Login</Link>
+            <h2 className="response">{response}</h2>
           </div>
-        </section>
-      ) : (
-        <section className="registration">
-          <h1>Registration</h1>
-          <form className="register_form" onSubmit={handleSubmit}>
-            <div>
-              <h2 className="response">{response}</h2>
-            </div>
-            <div>
-              <label htmlFor="name">Name</label>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                autoComplete="off"
-                placeholder="Enter your name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                placeholder="Enter your password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="confirmPassword">Confirm Password</label>
-              <input
-                type="password"
-                name="confirmPassword"
-                id="confirmPassword"
-                placeholder="Confirm your password"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                required
-              />
-            </div>
-            <div>
-              <button className="submit" type="submit">
-                Register
-              </button>
-            </div>
-          </form>
-        </section>
-      )}
-    </>
+          <div>
+            <label htmlFor="name" aria-hidden="false" className="label">
+              Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              autoComplete="off"
+              placeholder="Enter your name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password" aria-hidden="false" className="label">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="confirmPassword"
+              aria-hidden="false"
+              className="label"
+            >
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              name="confirmPassword"
+              id="confirmPassword"
+              placeholder="Confirm your password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <button className="submit" type="submit">
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
   );
 };
 
