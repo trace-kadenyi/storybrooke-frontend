174906cee722fc51899876c886596d3115357a58
Tracey Kadenyi <treykadenyi@gmail.com>
2023-03-25 15:33:20 +0300

    Add useLogout hook

---
+++ src/hooks/useLogout.js
@@ -0,0 +1,20 @@
+import axios from "../Api/axios";
+import useAuth from "./useAuth";
+
+const useLogout = () => {
+  const { setAuth } = useAuth();
+
+  const logout = async () => {
+    try {
+      const response = await axios("/logout", {
+        withCredentials: true,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return logout;
+};
+
+export default useLogout;
