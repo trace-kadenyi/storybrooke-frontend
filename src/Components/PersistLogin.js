e53f75a32d87346774795d4fd668255648238e1d
Tracey Kadenyi <treykadenyi@gmail.com>
2023-03-25 15:33:06 +0300

    Create component to persist login

---
+++ src/Components/PersistLogin.js
@@ -0,0 +1,38 @@
+import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import useRefreshToken from "../hooks/useRefreshToken";
+import useAuth from "../hooks/useAuth";
+
+const PersistLogin = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const refresh = useRefreshToken();
+  const { auth, persist } = useAuth();
+
+  useEffect(() => {
+    let isMounted = true;
+    const verifyRefreshToken = async () => {
+      try {
+        await refresh();
+      } catch (error) {
+        console.log(error);
+      } finally {
+        isMounted && setIsLoading(false);
+      }
+    };
+
+    !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
+
+    return () => (isMounted = false);
+  }, []);
+
+  useEffect(() => {
+    console.log(`isLoading: ${isLoading}`);
+    console.log(`auth: ${JSON.stringify(auth?.accessToken)}`);
+  }, [isLoading]);
+
+  return (
+    <>{!persist ? <Outlet /> : isLoading ? <p>Loading...</p> : <Outlet />}</>
+  );
+};
+
+export default PersistLogin;
