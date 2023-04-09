3e77ddbe48be319295c7e7bd3ebe770c4d5c12c5
Tracey Kadenyi <treykadenyi@gmail.com>
2023-04-09 15:21:19 +0300

    Create navigation

--- src/Components/Pages/Stories/StoriesPage.js
+++ src/Components/Pages/Stories/StoriesPage.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect, useContext, useState, useRef } from "react";
+import { useNavigate, NavLink, useLocation } from "react-router-dom";
+
+import logo from "../../../Assets/Images/logo.png";
+import Logout from "../../Logout";
+import "./stories.css"
 
 const StoriesPage = () => {
+  const navigate = useNavigate();
   return (
-    <div>StoriesPage</div>
-  )
-}
+    <section className="stories_sect">
+      <header className="login_header">
+        <nav>
+          <img src={logo} alt="logo" className="logo" />
+          <ul>
+            <li>
+              <NavLink to="/about" className="link">
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/editor" className="link">
+                Editor
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/Admin" className="link">
+                Admin
+              </NavLink>
+            </li>
+            <li>
+              <Logout />
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </section>
+  );
+};
 
-export default StoriesPage
+export default StoriesPage;
