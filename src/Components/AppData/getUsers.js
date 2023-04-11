2a6c68cf344d9e90aa9003b3d741dd685179ff02
Tracey Kadenyi <treykadenyi@gmail.com>
2023-04-11 22:01:03 +0300

    Store the function that fetches users in a single file

---
+++ src/Components/AppData/getUsers.js
@@ -0,0 +1,20 @@
+export const getUsers = async (setUsers, axiosPrivate, navigate, location) => {
+  let isMounted = true;
+  const controller = new AbortController();
+
+  try {
+    const response = await axiosPrivate.get("/users", {
+      signal: controller.signal,
+    });
+    // console.log(response.data);
+    isMounted && setUsers(response.data);
+  } catch (err) {
+    console.log(err);
+    navigate("/login", { state: { from: location }, replace: true });
+  }
+
+  return () => {
+    isMounted = false;
+    controller.abort();
+  };
+};
