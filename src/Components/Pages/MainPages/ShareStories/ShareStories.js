ae496487c63616384329fbc04e1ed36f0a9d3562
Tracey Kadenyi <treykadenyi@gmail.com>
2023-04-19 23:23:55 +0300

    Add functionality to create stories

--- src/Components/Pages/MainPages/ShareStories/ShareStories.js
+++ src/Components/Pages/MainPages/ShareStories/ShareStories.js
@@ -1,11 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink, Link } from "react-router-dom";
+import axios from "../../../../Api/axios";
 
 import Logout from "../../../Logout";
 import logo from "../../../../Assets/Images/logo.png";
+import { btnOptions } from "../../../AppData/data";
 import "./share.css";
+import useAxiosPrivate from "../../../../hooks/useAxiosPrivate";
 
 const ShareStories = () => {
+  const [genres, setGenres] = useState([]);
+  const axiosPrivate = useAxiosPrivate();
+  const [title, setTitle] = useState("");
+  const [story, setStory] = useState("");
+  const [author, setAuthor] = useState(
+    JSON.parse(localStorage.getItem("user"))
+  );
+  const [response, setResponse] = useState("");
+
   //    capitalize sentences
   const capitalize = (str) => {
     if (str.length === 0) {
@@ -19,6 +31,70 @@ const ShareStories = () => {
         return str.replace(/([.?!])\s*(\w)/g, (match, p1, p2) => {
           return p1 + " " + p2.toUpperCase();
         });
+      }
+    }
+  };
+
+  // toggle genre div when the button is clicked
+  const handleBtnClick = (e) => {
+    e.preventDefault();
+    const genreDiv = document.querySelector(".genre_checkboxes");
+    genreDiv.classList.toggle("visible");
+  };
+
+  // handle genre selection
+  const handleClick = (e) => {
+    e.preventDefault();
+    e.target.classList.toggle("selected");
+    if (genres.includes(e.target.innerText)) {
+      const index = genres.indexOf(e.target.innerText);
+      genres.splice(index, 1);
+    } else {
+      genres.push(e.target.innerText);
+    }
+    console.log(genres);
+  };
+
+  // handle author radio input
+  const handleAuthor = (e) => {
+    setAuthor(e.target.value);
+  };
+
+  // handle create story
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const storyData = {
+      title: title,
+      body: story,
+      author: author,
+      genres: genres,
+    };
+    console.log(storyData);
+    try {
+      const response = await axiosPrivate.post("/story", storyData);
+      console.log(response.data);
+      setResponse("Story published successfully");
+      setTitle("");
+      setStory("");
+      setAuthor(JSON.parse(localStorage.getItem("user")));
+      setGenres([]);
+      // remove selected class from genre buttons
+      const genreBtns = document.querySelectorAll(".genre_btn");
+      if(genreBtns.classList.contains("selected")){
+        genreBtns.classList.remove("selected");
+      }
+    } catch (error) {
+      if (!error.response) {
+        setResponse("No server response");
+      } else if (error.response.status === 401) {
+        setResponse("Unauthorized");
+      } else if (error.response.status === 400) {
+        setResponse(
+          "The story you're trying to add already exists in at least one genre in the database. Please come up with a new story. If you want to add this story to another genre, please use the update story route."
+        );
+      } else {
+        setResponse("Something went wrong");
       }
     }
   };
@@ -74,17 +150,81 @@ const ShareStories = () => {
         </ul>
       </div>
       <div className="share_stories_div">
-        <form className="share_stories_form">
-          <label htmlFor="title">Title</label>
-          <input type="text" name="title" id="title" />
-          <label htmlFor="genre">Genre</label>
-          <select name="genre" id="genre">
+        <form className="share_stories_form" onSubmit={handleSubmit}>
+          {/* title input */}
+          <div className="story_title">
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              name="title"
+              id="title"
+              placeholder="Enter your story title here"
+              required
+              value={title}
+              onChange={(e) => setTitle(capitalize(e.target.value))}
+            />
+          </div>
+
+          {/* radio input */}
+          <div className="genre_radio">
+            <p>
+              <strong>
+                Publish your story as <em>yourself</em> or as an{" "}
+                <em>anonymous author</em>?
+              </strong>
+            </p>
+            <input
+              type="radio"
+              name="owner"
+              id="owner"
+              value={JSON.parse(localStorage.getItem("user"))}
+              checked={author === JSON.parse(localStorage.getItem("user"))}
+              onChange={handleAuthor}
+            />
+            <label htmlFor="owner">
+              {JSON.parse(localStorage.getItem("user"))}
+            </label>
+            <input
+              type="radio"
+              name="anonymous"
+              id="anonymous"
+              value="Anonymous"
+              checked={author === "Anonymous"}
+              onChange={handleAuthor}
+            />
+            <label htmlFor="anonymous">Anonymous</label>
+          </div>
+          {/* <select name="genre" id="genre">
             <option value="romance">Romance</option>
             <option value="horror">Horror</option>
             <option value="comedy">Comedy</option>
             <option value="drama">Drama</option>
             <option value="action">Action</option>
-          </select>
+          </select> */}
+          <div className="genre_popup">
+            <p>
+              <strong>Choose a maximum of 3 genre(s) for your story</strong>
+            </p>
+            <button className="genre_selection_btn" onClick={handleBtnClick}>
+              Click to select genres
+            </button>
+            <div className="genre_checkboxes visible">
+              <div className="genre_btns">
+                {btnOptions.sort().map((btn, index) => {
+                  return (
+                    <button
+                      key={index}
+                      className="genre_btn"
+                      onClick={handleClick}
+                    >
+                      {btn}
+                    </button>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+
           <label htmlFor="story">Story</label>
           <textarea
             type="text"
@@ -92,14 +232,25 @@ const ShareStories = () => {
             id="story"
             rows="20"
             className="story_textarea"
-            required
             placeholder="Tell me a story..."
+            required
+            value={story}
+            onChange={(e) => setStory(capitalize(e.target.value))}
             // trigger the capitalize function on every keypress
             onKeyPress={(e) => {
               e.target.value = capitalize(e.target.value);
             }}
           ></textarea>
+
+          <div>
+            <button type="submit" className="share_btn">
+              Publish
+            </button>
+          </div>
         </form>
+        <p>
+          <strong>{response}</strong>
+        </p>
       </div>
     </section>
   );
