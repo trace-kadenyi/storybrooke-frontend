df0b721bc6e9889f847032235381b43f2b26d8cc
Tracey Kadenyi <treykadenyi@gmail.com>
2023-04-22 19:10:32 +0300

    Create read component

--- src/Components/Pages/MainPages/ReadStories/ReadStories.js
+++ src/Components/Pages/MainPages/ReadStories/ReadStories.js
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { NavLink, Link } from "react-router-dom";
+import { toast } from "react-toastify";
+
+import Logout from "../../../Logout";
+import logo from "../../../../Assets/Images/logo.png";
+import { btnOptions } from "../../../AppData/data";
+import useAxiosPrivate from "../../../../hooks/useAxiosPrivate";
+import "./read.css";
 
 const ReadStories = () => {
   return (
-    <div>ReadStories</div>
+    <section className='read_stories_sect'>
+       <header className="login_header">
+        <nav>
+          <Link to="/">
+            <img src={logo} alt="logo" className="logo" />
+          </Link>
+          <ul>
+            <li>
+              <NavLink to="/" className="link">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" className="link">
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/main" className="link">
+                Main
+              </NavLink>
+            </li>
+            <li>
+              <Logout />
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div>
+        <h1>Read Stories</h1>
+        <p>
+          Read some of the stories written by the users of this platform. 
+        </p>
+      </div>
+    </section>
   )
 }
 
