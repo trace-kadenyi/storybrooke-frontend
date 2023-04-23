998646b1e89531131fab8246d416e9196057f846
Tracey Kadenyi <treykadenyi@gmail.com>
2023-04-23 16:43:22 +0300

    Change link

--- src/Components/Pages/MainPages/ReadStories/ReadStories.js
+++ src/Components/Pages/MainPages/ReadStories/ReadStories.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { NavLink, Link } from "react-router-dom";
+import { NavLink, Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 import Logout from "../../../Logout";
@@ -9,9 +9,11 @@ import useAxiosPrivate from "../../../../hooks/useAxiosPrivate";
 import "./read.css";
 
 const ReadStories = () => {
+  const navigate = useNavigate();
+
   return (
-    <section className='read_stories_sect'>
-       <header className="login_header">
+    <section className="read_stories_sect">
+      <header className="login_header">
         <nav>
           <Link to="/">
             <img src={logo} alt="logo" className="logo" />
@@ -40,22 +42,31 @@ const ReadStories = () => {
       </header>
       <div>
         <>
-        <p className="read_stories_intro">
-          Read some of the stories written by the users of this platform. You can filter the stories by the genre of your choice, your favourite author or the title of the story. You can also explore all the stories by clicking on the "Explore" button. In an effort to customize your account, we have created a page dedicated to stories within the genres/interests you have selected. Click on the "My stories" button to view the stories that are relevant to you.
-        </p>
+          <p className="read_stories_intro">
+            Read some of the stories written by the users of this platform. You
+            can filter the stories by the genre of your choice, your favourite
+            author or the title of the story. You can also explore all the
+            stories by clicking on the "Explore" button. In an effort to
+            customize your account, we have created a page dedicated to stories
+            within the genres/interests you have selected. Click on the "My
+            stories" button to view the stories that are relevant to you.
+          </p>
         </>
         <div className="read_stories_btns">
-          <button className="read_stories_btn">Explore</button>
+          <button
+            className="read_stories_btn"
+            onClick={() => navigate("/explore")}
+          >
+            Explore
+          </button>
           <button className="read_stories_btn">My stories</button>
           <button className="read_stories_btn">By genre</button>
           <button className="read_stories_btn">By author</button>
           <button className="read_stories_btn">By title</button>
         </div>
-
-
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default ReadStories
+export default ReadStories;
