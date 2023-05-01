31100be238ff8b0799a47c16671af602e06852b5
Tracey Kadenyi <treykadenyi@gmail.com>
2023-05-01 20:48:27 +0300

    Create component functionality to fetch and display stories

--- src/Components/Pages/MainPages/ReadStories/MyStories.js
+++ src/Components/Pages/MainPages/ReadStories/MyStories.js
@@ -9,11 +9,85 @@ import "./read.css";
 
 const MyStories = () => {
   const [stories, setStories] = useState([]);
+  const [interests, setInterests] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [response, setResponse] = useState("");
   const [error, setError] = useState(null);
   const effectRun = useRef(false);
   const axiosPrivate = useAxiosPrivate();
-  
+
+  const currentUser = JSON.parse(localStorage.getItem("user"));
+
+  // fetch interests from user
+  useEffect(() => {
+    const getInterests = async () => {
+      try {
+        const response = await axiosPrivate.get(`/users`);
+        // return the interests of the logged in user
+        const userInterests = response.data.find(
+          (user) => user.username === currentUser
+        ).interests;
+        // set the interests in the state in alphabetical order
+        setInterests(
+          userInterests.sort((a, b) =>
+            a.toLowerCase().localeCompare(b.toLowerCase())
+          )
+        );
+        console.log(interests);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getInterests();
+    //eslint-disable-next-line
+  }, []);
+
+  // get stories based on interests/genres
+  useEffect(() => {
+    let isMounted = true;
+    const controller = new AbortController();
+
+    // get the stories from each genre in the interests array
+    const getStoriesFromInterests = async () => {
+      setLoading(true);
+      try {
+        const storiesFromInterests = await Promise.all(
+          interests.map(async (interest) => {
+            const response = await axiosPrivate.get(`/story/${interest}`, {
+              signal: controller.signal,
+            });
+            return response.data;
+          })
+        );
+
+        // remove responses with no stories
+        const filteredStories = storiesFromInterests.filter(
+          (story) => story.length > 0
+        );
+        filteredStories.flat();
+        isMounted && setStories(filteredStories.flat());
+        if (filteredStories.length === 0) {
+          setResponse("No stories found");
+        }
+        setLoading(false);
+
+        return filteredStories;
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    if (effectRun.current) {
+      getStoriesFromInterests();
+    }
+
+    return () => {
+      isMounted = false;
+      controller.abort();
+      effectRun.current = true;
+    };
+  }, []);
+
   return (
     <section className="explore_sect">
       <header className="login_header">
@@ -43,6 +117,41 @@ const MyStories = () => {
           </ul>
         </nav>
       </header>
+      <>
+        <div className="all_stories">
+          <p className="explore_intro">My Stories</p>
+          {loading ? (
+            <p className="load_stories">Loading...</p>
+          ) : stories.length > 0 ? (
+            stories.map((story) => {
+              return (
+                <div key={story._id} className="individual_story">
+                  <div className="title_date">
+                    <p className="title_author">
+                      <span className="story_title">{story.title} </span>
+                      <span className="by">by </span>
+                      <span className="story_author">{story.author}</span>
+                    </p>
+                    <span className="story_date">{story.date}</span>
+                  </div>
+                  <ul className="story_genres">
+                    {story.genres.map((genre) => {
+                      return (
+                        <li key={genre} className="story_genre">
+                          {genre} |
+                        </li>
+                      );
+                    })}
+                  </ul>
+                  <p className="story_body">{story.body}</p>
+                </div>
+              );
+            })
+          ) : (
+            <div className="by_genre_response">{response}</div>
+          )}
+        </div>
+      </>
     </section>
   );
 };
