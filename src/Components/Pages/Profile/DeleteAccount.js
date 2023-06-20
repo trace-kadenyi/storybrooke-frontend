d61e2227bbcf095be22645b85585b01905b311e4
Tracey Kadenyi <treykadenyi@gmail.com>
2023-06-20 17:03:09 +0300

    Create component for account deletion

--- src/Components/Pages/Profile/DeleteAccount.js
+++ src/Components/Pages/Profile/DeleteAccount.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import MainNavbar from "../../Navigation/MainNavbar";
 
 const DeleteAccount = () => {
   return (
-    <div>DeleteAccount</div>
-  )
-}
+    <section className="delete_acc_sect">
+      <MainNavbar />
+      <div className="delete_acc_main">
+        <>
+          <p className="sad_emoji">🥺</p>
+          <p className="goodbye">We're sorry to see you go...</p> <br />
+          <p className="goodbye_text">
+            All your data will be deleted from our database (except anonymous
+            stories). If you published any stories with us, we advice you to
+            back them up just in case you need to use them in the future.
+          </p>
+        </>
+        <div>
+          <button className="delete_account_permanently">Delete Account Permanently</button>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default DeleteAccount
+export default DeleteAccount;
