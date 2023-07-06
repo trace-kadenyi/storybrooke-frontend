bc414d9d20e5b555506e31d3f66fe5f89200d17f
Tracey Kadenyi <treykadenyi@gmail.com>
2023-07-06 19:01:17 +0300

    Refactor code

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = "https://imagine-tellmeastory-api.vercel.app"
+// const BASE_URL = "https://imagine-tellmeastory-api.vercel.app"
+
+const BASE_URL = "https://imagine-tellmeastory-api-git-dev-trace-kadenyi.vercel.app/"
 
 export default axios.create({
   baseURL: BASE_URL,
