f847a5d81917597123137cb44e0f42ef805348ce
Tracey Kadenyi <treykadenyi@gmail.com>
2023-07-11 13:33:17 +0300

    Create component to search for user profiles

---
+++ src/Components/Pages/Profile/UsersProfiles.js
@@ -0,0 +1,194 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+import MainNavbar from "../../Navigation/MainNavbar";
+import Blueprint from "../MainPages/ReadStories/Blueprint";
+import useAxiosPrivate from "../../../hooks/useAxiosPrivate";
+import "./profile.css";
+import preloader from "../../../Assets/Images/bio_preloader.gif";
+import profPicPreloader from "../../../Assets/Images/pic_preloader.gif";
+import storiesPreloader from "../../../Assets/Images/main.gif";
+import defaultCover from "../../../Assets/Images/about.png";
+
+const UsersProfiles = () => {
+  // make current user the name on the url
+  const currentUser = window.location.pathname.split("/")[2];
+  
+  const navigate = useNavigate();
+
+  const [profileInterests, setProfileInterests] = useState([]);
+  const [bio, setBio] = useState("");
+  const [profPic, setProfPic] = useState("");
+  const [coverPic, setCoverPic] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [dateJoined, setDateJoined] = useState("");
+  const [stories, setStories] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadProfile, setLoadProfile] = useState(false);
+  const [response, setResponse] = useState("");
+  const [error, setError] = useState(null);
+  const axiosPrivate = useAxiosPrivate();
+  const [active, setActive] = useState(0);
+  const [userProfile, setUserProfile] = useState({});
+
+  const controller = new AbortController();
+  // fetch profile details
+  useEffect(() => {
+    const fetchUserProfile = async () => {
+      try {
+        setLoadProfile(true);
+        const response = await axiosPrivate.get(`/profile/${currentUser}`);
+        setFirstName(response.data.firstname);
+        setLastName(response.data.lastname);
+        setUsername(response.data.username);
+        setBio(response.data.bio);
+        setProfPic(response.data.profilePicture);
+        response.data.coverPicture.includes("data:image")
+          ? setCoverPic(response.data.coverPicture)
+          : setCoverPic(defaultCover);
+        setDateJoined(response.data.dateJoined);
+        setLoadProfile(false);
+      } catch (err) {
+        console.log(err);
+        setError(error);
+        setLoadProfile(false);
+      }
+    };
+    fetchUserProfile();
+    //eslint-disable-next-line
+  }, [currentUser]);
+
+
+
+
+  return (
+    <section className="profile_sect">
+      <MainNavbar />
+      <div className="main_div">
+        {/* cover image */}
+        <div className="cover_div">
+          <img
+            src={coverPic ? coverPic : defaultCover}
+            className="cover_pic"
+            alt="cover_img"
+          />
+        </div>
+        {/* card with profile details */}
+        <div className="user_card">
+          <div className="user_card_div">
+            <div className="user_img">
+              <img
+                src={profPic ? profPic : profPicPreloader}
+                className={profPic ? "prof_pic" : "user_img_default"}
+                alt="user_img"
+              />
+            </div>
+         
+            <div className="bio">
+              {/* preloader */}
+              {loadProfile && (
+                <div className="preloader_div">
+                  {<img src={preloader} alt="preloader" />}
+                </div>
+              )}
+              <p>{bio}</p>
+            </div>
+          </div>
+          <div className="profile_username_div main_profile_username_div">
+            {!loadProfile && (
+              <div className="acc_details">
+                <div className="date_joined_div">
+                  <p className="date_joined">
+                    Joined:{" "}
+                    <span className="date_span">
+                      {dateJoined && dateJoined.slice(0, 10)}
+                    </span>
+                  </p>
+                </div>
+              </div>
+            )}
+            <div className="profile_names_div">
+              <p className="username_span_para">
+                <span className="username_span">{firstName}</span>
+                {/* space */} <span className="username_span">{lastName}</span>
+              </p>
+
+              {!loadProfile && <p className="profile_username">({username})</p>}
+            </div>
+          </div>
+        </div>
+
+        {/* profile contents */}
+        <div className="profile_content_main_div">
+          {/* <div className="links_container">
+            <button
+              className={active === 1 ? "tabs active_tabs" : "tabs"}
+              onClick={() => handleTabs(1)}
+            >
+              {currentUser}'s Interests
+            </button>
+            <button
+              className={active === 2 ? "tabs active_tabs" : "tabs"}
+              onClick={() => handleTabs(2)}
+            >
+              {currentUser}'s Stories
+            </button>
+          </div> */}
+
+          {/* interests */}
+          {/* <div className={active === 1 ? "active_content" : "content"}>
+            <div className="profile_interests_list">
+              {loading ? (
+                <div className="main_preloader">
+                  <img
+                    src={storiesPreloader}
+                    alt="preloader"
+                    // className="main_preloader_img"
+                  />
+                </div>
+              ) : (
+                profileInterests.map((interest) => (
+                  <div className="profile_interests_item" key={interest}>
+                    <p>{interest}</p>
+                  </div>
+                ))
+              )}
+            </div>
+          </div> */}
+          {/* stories */}
+          <div className="profile_stories_div">
+            {/* display found stories by the author */}
+            {/* <div>
+              <>
+                <div className="all_stories">
+                  {loading ? (
+                    <div className="main_preloader">
+                      <img
+                        src={storiesPreloader}
+                        alt="preloader"
+                        className="main_preloader_img"
+                      />
+                    </div>
+                  ) : stories.length > 0 ? (
+                    stories.map((story) => {
+                      return <Blueprint key={story._id} story={story} />;
+                    })
+                  ) : (
+                    <div className="by_genre_response">{response}</div>
+                  )}
+                </div>
+              </>
+            </div> */}
+          </div>
+        </div>
+
+
+      </div>
+    </section>
+  );
+};
+
+export default UsersProfiles;
